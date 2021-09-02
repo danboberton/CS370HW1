adb7fbed02264632afcf801904ffd572f4dff025
Dan Butcher <danbobbutcher@gmail.com>
2021-09-02 17:38:59 -0600

    Before help session, almost working

--- Initiator.c
+++ Initiator.c
@@ -1,2 +1,10 @@
 // Initiator.c
 
+int main(int argc, char** argv) {
+
+
+    // validate input
+    int seed = 
+    srand(seed);
+    printf("[Initiator]: With seed: %d\n", seed);   
+}
--- Worker.c
+++ Worker.c
@@ -1,31 +1,50 @@
 // Worker.c
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) {
 
-    size_t increment = 0;
     int** memArray = NULL;
     int* curInt = NULL;
+    char* arg2 = argv[1]; 
+    int numArray = atoi(arg2);
 
     // Validate input
-    if (argc < 1 || argc > 1 ){
-        printf("\nInvalid Input");
+    if (argc < 2 || argc > 2 ){
+        printf("\n[Worker]: Invalid Input\n");
+        return 0;
     }
 
+    if (numArray <= 0){
+        printf("\n[Worker]: Invalid Array Size\n");
+        return 0;
+    }
+    
+
     // Create mem pointer array
-    memArray = malloc(*argv[0] * sizeof(int*));
+    memArray = malloc(numArray * sizeof(int*));
 
     // allocate memory
     curInt = memArray;
 
-    for (;increment < *argv[0]; increment++){
+    for (size_t increment = 0;increment < numArray; increment++){
         curInt = malloc(sizeof(int));
-        *curInt = 
+        *curInt = random_in_range(1, 99);
+        curInt = curInt+1;
     }
+
+    // print memory
+   curInt = memArray;
+
+    for (size_t increment = 0;increment < numArray; increment++){
+        printf(*curInt);
+        curInt = curInt + 1;
+    } 
 
     //deallocate memory
 
-
+return 1;
 }
 
 int random_in_range(int lower_bound, int upper_bound){
